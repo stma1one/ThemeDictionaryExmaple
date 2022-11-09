5045dec50653d05dd074e002617e5793dd2978b9
User <User@TEACHER3>
2022-11-09 13:32:29 +0200

    Add project files.

---
+++ Theme/ConsoleTheme.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme
+{
+    public class ConsoleWindowSetting:ISettings
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public void Apply()
+        {
+             
+            Console.WindowWidth = width;
+            Console.WindowHeight = height;
+        }
+    }
+    public class ConsoleTheme : ISettings
+    {
+        public ConsoleColor BackgroundColor { get; set; }
+        public ConsoleColor FackgroundColor { get; set; }
+        public void Apply()
+        {
+            Console.ForegroundColor = FackgroundColor; 
+            Console.BackgroundColor = BackgroundColor;
+            Console.Clear();
+        }
+    }
+}
---
+++ Theme/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Theme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string,ISettings> settings = new Dictionary<string,ISettings>();
+            settings.Add("LightTheme", new ConsoleTheme() { BackgroundColor = ConsoleColor.White, FackgroundColor = ConsoleColor.Black });
+            settings.Add("DarkTheme", new ConsoleTheme() { BackgroundColor = ConsoleColor.Black, FackgroundColor = ConsoleColor.White });
+            settings.Add("Small", new ConsoleWindowSetting() { height = Console.WindowHeight/2, width = Console.WindowWidth/2 });
+            settings.Add("Big", new ConsoleWindowSetting() { height = Console.WindowHeight * 2, width = Console.WindowWidth * 2 });
+            while (true)
+            {
+                Console.WriteLine("Please choose: ");
+                Console.WriteLine("1- light, 2-dark, 3-window size 4- small size");
+                int choice;
+                while (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("1- light, 2-dark, 3-window size, 4- small size");
+                }
+                switch (choice)
+                {
+                    case 1:
+                        settings["LightTheme"].Apply();
+                        Console.WriteLine("Light Theme Applied");
+                        break;
+                    case 2:
+                        settings["DarkTheme"].Apply();
+                        Console.WriteLine("Dark Theme Applied");
+                        break;
+                    case 3:
+                        settings["Big"].Apply();
+                        Console.WriteLine("ffff");
+                        break;
+                    case 4:
+                        settings["Small"].Apply();
+                        break;
+                    default:
+                        {
+                            Console.ResetColor();
+                            
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
